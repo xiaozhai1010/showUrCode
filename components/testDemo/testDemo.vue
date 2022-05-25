574d5af362f341eefc37f6936f54e79d5167f107
xiaozhai1010 <xiaozhai1010@gmail.com>
2022-05-25 18:05:37 +0800

    feat:子父组件v-model 测试

---
+++ components/testDemo/testDemo.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<button @click="countAdd">click</button>
+		 add 1 when u click btn : {{curNum}}
+	</view>
+</template>
+
+<script>
+	export default {
+		props:['num'],
+		data() {
+			return {
+				curNum:0,
+			}
+		},
+		methods: {
+			countAdd(){
+				
+				this.$emit('addClick',this.curNum++)
+			}
+		},
+		watch:{
+			curNum:{
+				handler(val){
+					this.curNum = val
+				}
+			}
+		}
+	} 
+</script>
+
+<style>
+
+</style>
--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,22 +1,74 @@
 <template>
-	<view>
-		<uniImg :maxImgNum="8" :upImgUrl="arr"></uniImg>
+	<view >
+		<view>
+			<uniImg :maxImgNum="8" :upImgUrl="arr"></uniImg>
+			<button  @click="clickImg">上传图片</button>
+		</view>
+		<view v-for="(item,index) in imgArr"  :key="index" class="showImg">
+				<image :src="item" @click="preImg(item,index)"></image>
+		</view>
+		<!-- #ifdef H5 -->
+		<view class="">
+			H5页面显示 
+		</view>
+		<!-- #endif -->
+		<!-- #ifdef MP-WEIXIN -->
+		<view class="">
+			wechat页面显示
+			<navigator style="color:blue;cursor: pointer;" url="/pages/home/home" open-type="switchTab" >点击跳转主页</navigator>
+			<demo v-model="num"></demo>
+		</view>
+		<!-- #endif -->
 	</view>
 </template>
 
 <script>
+	import demo from '@/components/testDemo/testDemo.vue';
 	import uniImg from '@/components/uploadImg/uploadImg.vue'
 	export default {
 		components:{
-			uniImg
+			uniImg,
+			demo
 		},
 		data() {
 			return {
-				arr:','
+				// num:1,
+				arr:',',
+				imgArr:[]
 			}
 		},
 		methods: {
-			
+			// naviToHome(){
+			// 	console.log('跳转')
+			// 	uni.switchTab({
+			// 		url:'/pages/home/home'
+			// 	})
+			// },
+			clickBtn(num){
+				console.log(num,'数字')
+			},
+			clickImg(){
+				uni.chooseImage({
+					count:2,
+					sizeType: ['original', 'compressed'], 
+					success:(res)=>{
+						console.log(res)
+						this.imgArr = res.tempFilePaths
+					}
+				})
+			},
+			preImg(val,idx){
+				console.log(val,idx)
+				uni.previewImage({
+					current:idx,
+					urls:this.imgArr,
+					loop:true,//app
+					indicator:'default',//app
+					success:()=>{
+						console.log(`当前预览的是第${idx+1}张照片`)
+					}
+				})
+			}
 		}
 	}
 </script>
